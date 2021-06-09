b121812db461a2d67d1ed8d7609d9be2862f818c
Brian John <Brian@BrianJohn.attlocal.net>
2021-06-09 18:41:25 -0500

    Added changes with Alchemy, Migrate, Login, forms, Models, register, login

--- app/__init__.py
+++ app/__init__.py
@@ -2,8 +2,25 @@ from flask import Flask
 
 from config import Config
 
-app = Flask(__name__)
+#import for flask DB and Migrator
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
+#import for flask login
+from flask_login import LoginManager
+
+
+#Create intance of Flask class, name it app
+app = Flask(__name__)
+#Add configurations
 app.config.from_object(Config)
 
-from app import routes
+
+# Create db and migrator
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+
+login = LoginManager(app)
+login.login_view = 'login' #specify what page to laod for non-authencated users
+
+from app import routes, models
--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,13 @@
-from app import app
-from flask import render_template
+from app import app, db
+from flask import render_template, request, redirect, url_for, flash
+from werkzeug.security import check_password_hash
+from flask_login import login_user, logout_user
 
+# Import for Forms
+from app.forms import UserInfoForm, LoginForm
+
+#Import for models 
+from app.models import User
 
 @app.route('/')
 def index():
@@ -17,6 +24,57 @@ def index():
     return render_template('index.html', **context)
 
 
-@app.route('/register')
+@app.route('/register', methods=['GET', 'POST'])
 def register():
-    return render_template('register.html')
+    form = UserInfoForm()
+    context = {
+        'form': form
+    }
+    if request.method== 'POST' and form.validate():
+        # get information
+        username = form.username.data
+        password = form.password.data
+        email = form.email.data
+
+        # Create new instance of User
+        new_user = User(username, email, password)
+
+        #add user to db
+        db.session.add(new_user)
+        db.session.commit()
+
+        #flash success message
+        flash('Succesfully Registered', 'success')
+
+
+        return redirect(url_for('index'))
+    return render_template('register.html', **context)
+
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    context = {
+        'form' : form
+    }
+    if request.method== 'POST' and form.validate():
+        email = form.email.data
+        password = form.password.data
+
+        # Query database for user with email
+        user = User.query.filter_by(email=email).first()
+        # if no user, flash incorrect credentials 
+        if user is None or not check_password_hash(user.password, password):
+            flash('Incorrect Email/Password. Please try again', 'danger')
+            return redirect(url_for('login'))
+        # else
+        # log user in 
+        login_user(user)
+        # flash success message 
+        flash('You have successfully logged in', 'success')
+        return redirect(url_for('index'))
+        
+
+
+    return render_template('login.html', **context)
